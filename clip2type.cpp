d22b8944d841cf2738f84ebd7e25040ce2e44188
dheera <dheera@dheera.net>
2024-08-10 14:43:33 -0700

    use lut instead of map

--- clip2type.cpp
+++ clip2type.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 #include <unistd.h>
 #include <linux/uinput.h>
 #include <fcntl.h>
@@ -11,191 +12,174 @@
 // the mapping formats below are:
 // [char that we want] -> { [uinput key that needs to be hit to get that char], [whether shift needs to be pressed] }
 
+constexpr std::pair<int, bool> default_key = {0, false};
+
 // QWERTY key mapping
-std::map<char, std::pair<int, bool>> qwerty_key_mapping = {
-    {'A', {KEY_A, true}},
-    {'B', {KEY_B, true}},
-    {'C', {KEY_C, true}},
-    {'D', {KEY_D, true}},
-    {'E', {KEY_E, true}},
-    {'F', {KEY_F, true}},
-    {'G', {KEY_G, true}},
-    {'H', {KEY_H, true}},
-    {'I', {KEY_I, true}},
-    {'J', {KEY_J, true}},
-    {'K', {KEY_K, true}},
-    {'L', {KEY_L, true}},
-    {'M', {KEY_M, true}},
-    {'N', {KEY_N, true}},
-    {'O', {KEY_O, true}},
-    {'P', {KEY_P, true}},
-    {'Q', {KEY_Q, true}},
-    {'R', {KEY_R, true}},
-    {'S', {KEY_S, true}},
-    {'T', {KEY_T, true}},
-    {'U', {KEY_U, true}},
-    {'V', {KEY_V, true}},
-    {'W', {KEY_W, true}},
-    {'X', {KEY_X, true}},
-    {'Y', {KEY_Y, true}},
-    {'Z', {KEY_Z, true}},
-    {'a', {KEY_A, false}},
-    {'b', {KEY_B, false}},
-    {'c', {KEY_C, false}},
-    {'d', {KEY_D, false}},
-    {'e', {KEY_E, false}},
-    {'f', {KEY_F, false}},
-    {'g', {KEY_G, false}},
-    {'h', {KEY_H, false}},
-    {'i', {KEY_I, false}},
-    {'j', {KEY_J, false}},
-    {'k', {KEY_K, false}},
-    {'l', {KEY_L, false}},
-    {'m', {KEY_M, false}},
-    {'n', {KEY_N, false}},
-    {'o', {KEY_O, false}},
-    {'p', {KEY_P, false}},
-    {'q', {KEY_Q, false}},
-    {'r', {KEY_R, false}},
-    {'s', {KEY_S, false}},
-    {'t', {KEY_T, false}},
-    {'u', {KEY_U, false}},
-    {'v', {KEY_V, false}},
-    {'w', {KEY_W, false}},
-    {'x', {KEY_X, false}},
-    {'y', {KEY_Y, false}},
-    {'z', {KEY_Z, false}},
-    {'0', {KEY_0, false}},
-    {'1', {KEY_1, false}},
-    {'2', {KEY_2, false}},
-    {'3', {KEY_3, false}},
-    {'4', {KEY_4, false}},
-    {'5', {KEY_5, false}},
-    {'6', {KEY_6, false}},
-    {'7', {KEY_7, false}},
-    {'8', {KEY_8, false}},
-    {'9', {KEY_9, false}},
-    {' ', {KEY_SPACE, false}},
-    {'\n', {KEY_ENTER, false}},
-    {',', {KEY_COMMA, false}},
-    {'.', {KEY_DOT, false}},
-    {'/', {KEY_SLASH, false}},
-    {'-', {KEY_MINUS, false}},
-    {'=', {KEY_EQUAL, false}},
-    {';', {KEY_SEMICOLON, false}},
-    {'\'', {KEY_APOSTROPHE, false}},
-    {'[', {KEY_LEFTBRACE, false}},
-    {']', {KEY_RIGHTBRACE, false}},
-    {'\\', {KEY_BACKSLASH, false}},
-    {'!', {KEY_1, true}},
-    {'@', {KEY_2, true}},
-    {'#', {KEY_3, true}},
-    {'$', {KEY_4, true}},
-    {'%', {KEY_5, true}},
-    {'^', {KEY_6, true}},
-    {'&', {KEY_7, true}},
-    {'*', {KEY_8, true}},
-    {'(', {KEY_9, true}},
-    {')', {KEY_0, true}},
-    {'_', {KEY_MINUS, true}},
-    {'+', {KEY_EQUAL, true}},
-    {':', {KEY_SEMICOLON, true}},
-    {'"', {KEY_APOSTROPHE, true}},
-    {'<', {KEY_COMMA, true}},
-    {'>', {KEY_DOT, true}},
-    {'?', {KEY_SLASH, true}},
-    {'{', {KEY_LEFTBRACE, true}},
-    {'}', {KEY_RIGHTBRACE, true}},
-    {'|', {KEY_BACKSLASH, true}},
-};
 
-std::map<char, std::pair<int, bool>> dvorak_key_mapping = {
-    {'A', {KEY_A, true}},
-    {'O', {KEY_S, true}},
-    {'E', {KEY_D, true}},
-    {'U', {KEY_F, true}},
-    {'I', {KEY_G, true}},
-    {'D', {KEY_H, true}},
-    {'H', {KEY_J, true}},
-    {'T', {KEY_K, true}},
-    {'N', {KEY_L, true}},
-    {'S', {KEY_SEMICOLON, true}},
-    {'\'', {KEY_Q, true}},
-    {',', {KEY_W, true}},
-    {'.', {KEY_E, true}},
-    {'P', {KEY_R, true}},
-    {'Y', {KEY_T, true}},
-    {'F', {KEY_Y, true}},
-    {'G', {KEY_U, true}},
-    {'C', {KEY_I, true}},
-    {'R', {KEY_O, true}},
-    {'L', {KEY_P, true}},
-    {'/', {KEY_LEFTBRACE, true}},
-    {'=', {KEY_RIGHTBRACE, true}},
-    {';', {KEY_Z, true}},
-    {'Q', {KEY_X, true}},
-    {'J', {KEY_C, true}},
-    {'K', {KEY_V, true}},
-    {'X', {KEY_B, true}},
-    {'B', {KEY_N, true}},
-    {'M', {KEY_M, true}},
-    {'W', {KEY_COMMA, true}},
-    {'V', {KEY_DOT, true}},
-    {'Z', {KEY_SLASH, true}},
-    {'a', {KEY_A, false}},
-    {'o', {KEY_S, false}},
-    {'e', {KEY_D, false}},
-    {'u', {KEY_F, false}},
-    {'i', {KEY_G, false}},
-    {'d', {KEY_H, false}},
-    {'h', {KEY_J, false}},
-    {'t', {KEY_K, false}},
-    {'n', {KEY_L, false}},
-    {'s', {KEY_SEMICOLON, false}},
-    {'-', {KEY_APOSTROPHE, false}},
-    {'_', {KEY_APOSTROPHE, true}},
-    {'\'', {KEY_Q, false}},
-    {',', {KEY_W, false}},
-    {'.', {KEY_E, false}},
-    {'p', {KEY_R, false}},
-    {'y', {KEY_T, false}},
-    {'f', {KEY_Y, false}},
-    {'g', {KEY_U, false}},
-    {'c', {KEY_I, false}},
-    {'r', {KEY_O, false}},
-    {'l', {KEY_P, false}},
-    {'/', {KEY_LEFTBRACE, false}},
-    {'=', {KEY_RIGHTBRACE, false}},
-    {';', {KEY_Z, false}},
-    {'q', {KEY_X, false}},
-    {'j', {KEY_C, false}},
-    {'k', {KEY_V, false}},
-    {'x', {KEY_B, false}},
-    {'b', {KEY_N, false}},
-    {'m', {KEY_M, false}},
-    {'w', {KEY_COMMA, false}},
-    {'v', {KEY_DOT, false}},
-    {'z', {KEY_SLASH, false}},
-    {' ', {KEY_SPACE, false}},
-    {'\n', {KEY_ENTER, false}},
-    {'\"', {KEY_Q, true}},
-    {'<', {KEY_W, true}},
-    {'>', {KEY_E, true}},
-    {'?', {KEY_LEFTBRACE, true}},
-    {'+', {KEY_RIGHTBRACE, true}},
-    {':', {KEY_Z, true}},
-    {'0', {KEY_0, false}},
-    {'1', {KEY_1, false}},
-    {'2', {KEY_2, false}},
-    {'3', {KEY_3, false}},
-    {'4', {KEY_4, false}},
-    {'5', {KEY_5, false}},
-    {'6', {KEY_6, false}},
-    {'7', {KEY_7, false}},
-    {'8', {KEY_8, false}},
-    {'9', {KEY_9, false}},
-};
+constexpr std::vector<std::pair<int, bool>> generate_qwerty_key_mapping() {
+    std::vector<std::pair<int, bool>> mapping(256, default_key);
 
+    mapping['A'] = {KEY_A, false};
+    mapping['B'] = {KEY_B, false};
+    mapping['C'] = {KEY_C, false};
+    mapping['D'] = {KEY_D, false};
+    mapping['E'] = {KEY_E, false};
+    mapping['F'] = {KEY_F, false};
+    mapping['G'] = {KEY_G, false};
+    mapping['H'] = {KEY_H, false};
+    mapping['I'] = {KEY_I, false};
+    mapping['J'] = {KEY_J, false};
+    mapping['K'] = {KEY_K, false};
+    mapping['L'] = {KEY_L, false};
+    mapping['M'] = {KEY_M, false};
+    mapping['N'] = {KEY_N, false};
+    mapping['O'] = {KEY_O, false};
+    mapping['P'] = {KEY_P, false};
+    mapping['Q'] = {KEY_Q, false};
+    mapping['R'] = {KEY_R, false};
+    mapping['S'] = {KEY_S, false};
+    mapping['T'] = {KEY_T, false};
+    mapping['U'] = {KEY_U, false};
+    mapping['V'] = {KEY_V, false};
+    mapping['W'] = {KEY_W, false};
+    mapping['X'] = {KEY_X, false};
+    mapping['Y'] = {KEY_Y, false};
+    mapping['Z'] = {KEY_Z, false};
+    mapping['a'] = {KEY_A, false};
+    mapping['b'] = {KEY_B, false};
+    mapping['c'] = {KEY_C, false};
+    mapping['d'] = {KEY_D, false};
+    mapping['e'] = {KEY_E, false};
+    mapping['f'] = {KEY_F, false};
+    mapping['g'] = {KEY_G, false};
+    mapping['h'] = {KEY_H, false};
+    mapping['i'] = {KEY_I, false};
+    mapping['j'] = {KEY_J, false};
+    mapping['k'] = {KEY_K, false};
+    mapping['l'] = {KEY_L, false};
+    mapping['m'] = {KEY_M, false};
+    mapping['n'] = {KEY_N, false};
+    mapping['o'] = {KEY_O, false};
+    mapping['p'] = {KEY_P, false};
+    mapping['q'] = {KEY_Q, false};
+    mapping['r'] = {KEY_R, false};
+    mapping['s'] = {KEY_S, false};
+    mapping['t'] = {KEY_T, false};
+    mapping['u'] = {KEY_U, false};
+    mapping['v'] = {KEY_V, false};
+    mapping['w'] = {KEY_W, false};
+    mapping['x'] = {KEY_X, false};
+    mapping['y'] = {KEY_Y, false};
+    mapping['z'] = {KEY_Z, false};
+    mapping['0'] = {KEY_0, false};
+    mapping['1'] = {KEY_1, false};
+    mapping['2'] = {KEY_2, false};
+    mapping['3'] = {KEY_3, false};
+    mapping['4'] = {KEY_4, false};
+    mapping['5'] = {KEY_5, false};
+    mapping['6'] = {KEY_6, false};
+    mapping['7'] = {KEY_7, false};
+    mapping['8'] = {KEY_8, false};
+    mapping['9'] = {KEY_9, false};
+    mapping[' '] = {KEY_SPACE, false};
+    mapping['\n'] = {KEY_ENTER, false};
+    mapping[','] = {KEY_COMMA, false};
+    mapping['.'] = {KEY_DOT, false};
+    mapping['/'] = {KEY_SLASH, false};
+    mapping['-'] = {KEY_MINUS, false};
+    mapping['='] = {KEY_EQUAL, false};
+    mapping[';'] = {KEY_SEMICOLON, false};
+    mapping['\''] = {KEY_APOSTROPHE, false};
+    mapping['['] = {KEY_LEFTBRACE, false};
+    mapping[']'] = {KEY_RIGHTBRACE, false};
+    mapping['\\'] = {KEY_BACKSLASH, false};
+    mapping['!'] = {KEY_1, true};
+    mapping['@'] = {KEY_2, true};
+    mapping['#'] = {KEY_3, true};
+    mapping['$'] = {KEY_4, true};
+    mapping['%'] = {KEY_5, true};
+    mapping['^'] = {KEY_6, true};
+    mapping['&'] = {KEY_7, true};
+    mapping['*'] = {KEY_8, true};
+    mapping['('] = {KEY_9, true};
+    mapping[')'] = {KEY_0, true};
+    mapping['_'] = {KEY_MINUS, true};
+    mapping['+'] = {KEY_EQUAL, true};
+    mapping[':'] = {KEY_SEMICOLON, true};
+    mapping['"'] = {KEY_APOSTROPHE, true};
+    mapping['<'] = {KEY_COMMA, true};
+    mapping['>'] = {KEY_DOT, true};
+    mapping['?'] = {KEY_SLASH, true};
+    mapping['{'] = {KEY_LEFTBRACE, true};
+    mapping['}'] = {KEY_RIGHTBRACE, true};
+    mapping['|'] = {KEY_BACKSLASH, true};
+
+    return mapping;
+}
+
+constexpr std::vector<std::pair<int, bool>> generate_dvorak_key_mapping() {
+    std::vector<std::pair<int, bool>> mapping(256, default_key);
+
+    mapping['A'] = {KEY_A, true};
+    mapping['O'] = {KEY_S, true};
+    mapping['E'] = {KEY_D, true};
+    mapping['U'] = {KEY_F, true};
+    mapping['I'] = {KEY_G, true};
+    mapping['D'] = {KEY_H, true};
+    mapping['H'] = {KEY_J, true};
+    mapping['T'] = {KEY_K, true};
+    mapping['N'] = {KEY_L, true};
+    mapping['S'] = {KEY_SEMICOLON, true};
+    mapping['\''] = {KEY_Q, true};
+    mapping[','] = {KEY_W, true};
+    mapping['.'] = {KEY_E, true};
+    mapping['P'] = {KEY_R, true};
+    mapping['Y'] = {KEY_T, true};
+    mapping['F'] = {KEY_Y, true};
+    mapping['G'] = {KEY_U, true};
+    mapping['C'] = {KEY_I, true};
+    mapping['R'] = {KEY_O, true};
+    mapping['L'] = {KEY_P, true};
+    mapping['/'] = {KEY_LEFTBRACE, true};
+    mapping['='] = {KEY_RIGHTBRACE, true};
+    mapping[';'] = {KEY_Z, true};
+    mapping['Q'] = {KEY_X, true};
+    mapping['J'] = {KEY_C, true};
+    mapping['K'] = {KEY_V, true};
+    mapping['X'] = {KEY_B, true};
+    mapping['B'] = {KEY_N, true};
+    mapping['M'] = {KEY_M, true};
+    mapping['W'] = {KEY_COMMA, true};
+    mapping['V'] = {KEY_DOT, true};
+    mapping['Z'] = {KEY_SLASH, true};
+    mapping['a'] = {KEY_A, false};
+    mapping['o'] = {KEY_S, false};
+    mapping['e'] = {KEY_D, false};
+    mapping['u'] = {KEY_F, false};
+    mapping['i'] = {KEY_G, false};
+    mapping['d'] = {KEY_H, false};
+    mapping['h'] = {KEY_J, false};
+    mapping['t'] = {KEY_K, false};
+    mapping['n'] = {KEY_L, false};
+    mapping['s'] = {KEY_SEMICOLON, false};
+    mapping['-'] = {KEY_APOSTROPHE, false};
+    mapping['_'] = {KEY_APOSTROPHE, true};
+    mapping['\''] = {KEY_Q, false};
+    mapping[','] = {KEY_W, false};
+    mapping['.'] = {KEY_E, false};
+    mapping['p'] = {KEY_R, false};
+    mapping['y'] = {KEY_T, false};
+    mapping['f'] = {KEY_Y, false};
+    mapping['g'] = {KEY_U, false};
+    mapping['c'] = {KEY_I, false};
+    mapping['r'] = {KEY_O, false};
+    mapping['l'] = {KEY_P, false};
+    mapping['/'] = {KEY_LEFTBRACE, true};
+    mapping['}'] = {KEY_RIGHTBRACE, true};
+    mapping['|'] = {KEY_BACKSLASH, true};
+
+    return mapping;
+}
 
 std::string get_clipboard_content() {
     char buffer[128];
@@ -214,14 +198,14 @@ std::string get_clipboard_content() {
     return result;
 }
 
-void send_keystrokes(const std::string& content, int fd, int delay_ms, std::map<char, std::pair<int, bool>>& key_mapping) {
+void send_keystrokes(const std::string& content, int fd, const std::vector<std::pair<int, bool>>& key_mapping, int delay_ms) {
     struct input_event ev;
 
     for (char ch : content) {
-        auto key_pair = key_mapping.find(ch);
-        if (key_pair != key_mapping.end()) {
-            int key = key_pair->second.first;
-            bool shift = key_pair->second.second;
+        if (static_cast<unsigned char>(ch) < key_mapping.size()) {
+            auto key_pair = key_mapping[static_cast<unsigned char>(ch)];
+            int key = key_pair.first;
+            bool shift = key_pair.second;
 
             if (shift) {
                 memset(&ev, 0, sizeof(struct input_event));
@@ -274,28 +258,30 @@ void send_keystrokes(const std::string& content, int fd, int delay_ms, std::map<
 
 int main(int argc, char* argv[]) {
     int delay_ms = 0; // Default delay is 0 ms
-    std::map<char, std::pair<int, bool>>* key_mapping = &qwerty_key_mapping; // Default to QWERTY
+    bool use_dvorak = false; // Default to QWERTY
 
     // Option parsing
     int opt;
-    while ((opt = getopt(argc, argv, "d:l:")) != -1) {
+    while ((opt = getopt(argc, argv, "d:k:")) != -1) {
         switch (opt) {
             case 'd':
                 delay_ms = atoi(optarg);
                 break;
-            case 'l':
-                if (std::string(optarg) == "dvorak") {
-                    key_mapping = &dvorak_key_mapping;
-                } else if (std::string(optarg) != "qwerty") {
-                    std::cerr << "Unknown layout: " << optarg << std::endl;
-                    return EXIT_FAILURE;
+            case 'k':
+                if (strcmp(optarg, "dvorak") == 0) {
+                    use_dvorak = true;
                 }
                 break;
             default:
-                std::cerr << "Usage: " << argv[0] << " [-d delay_in_ms] [-l layout (qwerty|dvorak)]" << std::endl;
+                std::cerr << "Usage: " << argv[0] << " [-d delay_in_ms] [-k keyboard_layout]" << std::endl;
+                std::cerr << "Keyboard layout options: qwerty, dvorak" << std::endl;
                 return EXIT_FAILURE;
         }
     }
+
+    const auto qwerty_mapping = generate_qwerty_key_mapping();
+    const auto dvorak_mapping = generate_dvorak_key_mapping();
+    const auto& key_mapping = use_dvorak ? dvorak_mapping : qwerty_mapping;
 
     int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
     if (fd < 0) {
@@ -306,8 +292,8 @@ int main(int argc, char* argv[]) {
     ioctl(fd, UI_SET_EVBIT, EV_KEY);
     ioctl(fd, UI_SET_EVBIT, EV_SYN);
 
-    for (const auto& key : *key_mapping) {
-        ioctl(fd, UI_SET_KEYBIT, key.second.first);
+    for (const auto& key : key_mapping) {
+        ioctl(fd, UI_SET_KEYBIT, key.first);
     }
     ioctl(fd, UI_SET_KEYBIT, KEY_LEFTSHIFT);
 
@@ -324,16 +310,10 @@ int main(int argc, char* argv[]) {
     sleep(1); // Wait for the device to be ready
 
     std::string clipboard_content = get_clipboard_content();
-    send_keystrokes(clipboard_content, fd, delay_ms, *key_mapping);
+    send_keystrokes(clipboard_content, fd, key_mapping, delay_ms);
 
     ioctl(fd, UI_DEV_DESTROY);
     close(fd);
 
     return EXIT_SUCCESS;
 }
-
-
-
-
-
-
--- make
+++ make
@@ -2,6 +2,6 @@
 
 # Lasy to write a makefile right now
 
-g++ -o clip2type clip2type.cpp -std=c++11
+g++ -o clip2type clip2type.cpp -std=c++2b
 sudo cp clip2type /usr/bin/
 sudo chmod 4755 /usr/bin/clip2type
